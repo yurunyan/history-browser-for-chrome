fd6fdedff270b5a87e1d5eda7f67b2ff30eb94c3
anonymous <anonymous@ho.ge>
2019-09-14 09:03:11 +0900

    auto

--- nicovideo_wx.py
+++ nicovideo_wx.py
@@ -8,13 +8,15 @@ from contextlib import closing
 import tempfile
 from glob import glob
 
+import nicovideo
+
 class App(wx.Frame):
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title, size=(500, 700), style=wx.DEFAULT_FRAME_STYLE)
         p = wx.Panel(self, wx.ID_ANY)
         layout = wx.BoxSizer(wx.VERTICAL)
         
-        self.button1 = wx.Button(p, wx.ID_ANY, 'ダウンロードフォルダをチェック')
+        self.button1 = wx.Button(p, wx.ID_ANY, 'チェック')
         self.button1.Bind(wx.EVT_BUTTON, self.btn1push)
         layout.Add(self.button1, flag=wx.EXPAND | wx.ALL, border=15)
         
@@ -33,20 +35,10 @@ class App(wx.Frame):
         self.Show()
         
     def btn1push(self, event):
-        dbfilelist = [expanduser("~/AppData/Local/Google/Chrome/User Data/Default/History")]
-        dbfilelist += glob(expanduser("~/AppData/Local/Google/Chrome/User Data/Profile */History"))
-        ls = []
-        with tempfile.TemporaryDirectory() as t:
-            for filefrom in dbfilelist:
-                fileto = join(t, 'temp.db')
-                shutil.copyfile(filefrom, fileto)
-                join(fileto, 'temp.db')
-                with closing(sqlite3.connect(fileto)) as conn:
-                    c = conn.cursor()
-                    key = "http%://%nicovideo.jp/watch/%"
-                    for x in c.execute('select url, last_visit_time from urls where url like ?', [key]):
-                        ls.append(','.join([x[0].split('?')[0], str(x[1])]))
-        self.text_entry.SetValue('\n'.join(ls))
+        v = ''
+        for row in nicovideo.search_nicovideo():
+            v += ''.join([str(x) for x in row]) + "\n"
+        self.text_entry.SetValue(v)
 
 
 app = wx.App()
